c586df9001685396a9759efd6735c964e1081a05
Rafael Viscarra <rafael.viscarra97@hotmail.com>
2023-02-05 16:12:08 -0600

    feat: add documentation for book enpoints

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,6 +10,55 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BookController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Get(
+     *   tags={"Book"},
+     *   path="/books",
+     *   security={{"bearerAuth":{}}},
+     *   @OA\Response(
+     *       response=200,
+     *       description="List of books",
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *               type="array",
+     *               @OA\Items(
+     *                     type="object",
+     *                     @OA\Property(
+     *                         property="id",
+     *                         description="Book id",
+     *                         type="integer",
+     *                         example=1
+     *                    ),
+     *                    @OA\Property(
+     *                         property="title",
+     *                         description="Book title",
+     *                         type="string",
+     *                         example="Title"
+     *                    ),
+     *                    @OA\Property(
+     *                         property="author",
+     *                         description="Book author",
+     *                         type="string",
+     *                         example="Author"
+     *                    ),
+     *                    @OA\Property(
+     *                         property="genre",
+     *                         description="Book genre name",
+     *                         type="string",
+     *                         example="Genre"
+     *                    ),
+     *               ),
+     *          ),
+     *      ),
+     *   ),
+     * ),
+     */
     public function index()
     {
         /** @var User $user */
@@ -20,6 +69,75 @@ class BookController extends Controller
         return response(BookResponse::collection($bookCollection), Response::HTTP_OK);
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Post(
+     *   tags={"Book"},
+     *   path="/books",
+     *   security={{"bearerAuth":{}}},
+     *   @OA\RequestBody(
+     *      required=true,
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *              @OA\Property(
+     *                  property="title",
+     *                  description="Book title",
+     *                  type="string",
+     *              ),
+     *              @OA\Property(
+     *                  property="author",
+     *                  description="Book author",
+     *                  type="string",
+     *              ),
+     *              @OA\Property(
+     *                  property="genre_id",
+     *                  description="Book genre id",
+     *                  type="integer",
+     *              ),
+     *          ),
+     *      ),
+     *  ),
+     *   @OA\Response(
+     *       response=201,
+     *       description="Book created",
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *                     @OA\Property(
+     *                         property="id",
+     *                         description="Book id",
+     *                         type="integer",
+     *                         example=1
+     *                    ),
+     *                    @OA\Property(
+     *                         property="title",
+     *                         description="Book title",
+     *                         type="string",
+     *                         example="Title"
+     *                    ),
+     *                    @OA\Property(
+     *                         property="author",
+     *                         description="Book author",
+     *                         type="string",
+     *                         example="Author"
+     *                    ),
+     *                    @OA\Property(
+     *                         property="genre",
+     *                         description="Book genre name",
+     *                         type="string",
+     *                         example="Genre"
+     *                    ),
+     *          ),
+     *      ),
+     *   )
+     * )
+     */
     public function store(BookRequest $request)
     {
         $bookInformation = $request->all();
@@ -32,6 +150,37 @@ class BookController extends Controller
         return response(new BookResponse($book->load('genre')), Response::HTTP_CREATED);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Delete(
+     *   tags={"Book"},
+     *   path="/books/{book_id}",
+     *   security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="book_id",
+     *         description="Book id to delete",
+     *         in = "path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *   @OA\Response(
+     *       response=204,
+     *       description="Returns empty body",
+     *      @OA\MediaType(
+     *          mediaType="application/json",
+     *          @OA\Schema(
+     *          ),
+     *      ),
+     *   )
+     * )
+     */
     public function destroy(Book $book)
     {
         /** @var User $user */
